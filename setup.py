145ec6bd6b54ce61290e59e123151d147b222ad2
Quastrado <you@example.com>
2020-10-25 23:17:56 +0300

    change of version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='Quastrado_check_type_wrapper',
-    version='0.1',
+    version='0.1.4',
     author='Quastrado',
     author_email='quastrado@gmail.com', 
     description='Package Description',
