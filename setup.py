622d88c36ddf796115079ffd4f77221eb61a2678
Quastrado <you@example.com>
2020-10-26 21:16:44 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 from setuptools import find_packages, setup
 
 
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
 setup(
     name='Quastrado_check_type_wrapper',
     version='0.1.4',
     author='Quastrado',
     author_email='quastrado@gmail.com', 
-    description='Package Description',
-    url='',
+    description='A simple decorator to control the type matching of the arguments passed',
+    long_description = long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/Quastrado/check_type_wrapper',
     packages=find_packages()
 )
