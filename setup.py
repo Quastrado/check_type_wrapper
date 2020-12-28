20c1e9c2114ca7e1ad486ea4aea71a7f107b07dc
Quastrado <you@example.com>
2020-12-29 02:32:51 +0300

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='Quastrado_check_type_wrapper',
-    version='0.1.4',
+    version='0.1.5',
     author='Quastrado',
     author_email='quastrado@gmail.com',
     description='''
