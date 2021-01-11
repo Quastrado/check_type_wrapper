5199bde4c827f6f8bd6f34a08ea959586dffbb3a
Quastrado <quastrado@gmail.com>
2021-01-12 01:40:12 +0300

    setup.py update

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import versioneer
 
 with open('README.md', 'r') as fh:
     long_description = fh.read()
+with open('requirements.txt', 'r') as fh:
+    requirements = [line.strip() for line in fh]
 
 
 setup(
