0732498a2fb256685930fd5980573b5ce4f9bf02
Quastrado <quastrado@gmail.com>
2021-01-20 03:25:16 +0300

    add imports

--- check_type_wrapper/__init__.py
+++ check_type_wrapper/__init__.py
@@ -1,3 +1,7 @@
+from .check_type import check_type
+from .discrepancy_item import DiscrepancyItem
+from .exception import TypeMissMatchException
+
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
